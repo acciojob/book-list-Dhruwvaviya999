f35eec97a9edba16ea2f6aed23691af195512987
Dhruw Vaviya <dhruwvaviya123@gmail.com>
2025-03-18 10:19:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 //your JS code here. If required.
+
+function addBook(){
+	let title = document.getElementById("title").value;
+	let author = document.getElementById("author").value;
+	let isbn = document.getElementById("isbn").value;
+
+	let tbody = document.getElementById("book-list");
+
+	let newRow = tbody.insertRow(0);
+	
+	let cell1 = newRow.insertCell(0);
+	let cell2 = newRow.insertCell(1);
+	let cell3 = newRow.insertCell(2);
+	let cell4 = newRow.insertCell(3);
+
+	cell1.innerHTML = title;
+	cell2.innerHTML = author;
+	cell3.innerHTML = isbn;
+	cell4.innerHTML = "<button>X</button>"
+
+	title = "";
+	author = "";
+	isbn = "";
+}
